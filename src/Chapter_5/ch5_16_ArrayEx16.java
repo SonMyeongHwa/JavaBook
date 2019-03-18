91a0e9ba4da71df2ced5f8fa67fbbb1c38efa356
smh01 <rheiddl@naver.com>
2019-03-19 04:06:16 +0900

    coding: Chapter_5

---
+++ src/Chapter_5/ch5_16_ArrayEx16.java
@@ -0,0 +1,10 @@
+package Chapter_5;
+
+public class ch5_16_ArrayEx16 {
+    public static void main(String[] args) {
+        System.out.println("매개변수의 개수:"+args.length);
+        for(int i=0; i<args.length; i++){
+            System.out.println("args["+i+"] = \""+args[i] + "\"");
+        }
+    }
+}
